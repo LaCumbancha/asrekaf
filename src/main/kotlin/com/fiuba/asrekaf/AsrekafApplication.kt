0002c6260ab428d9d42096b14484329b4c73d090
Cristian Raña <cristian.rana@despegar.com>
2020-05-26 21:21:58 -0300

    Server bootstrapping

---
+++ src/main/kotlin/com/fiuba/asrekaf/AsrekafApplication.kt
@@ -0,0 +1,11 @@
+package com.fiuba.asrekaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsrekafApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsrekafApplication>(*args)
+}
---
+++ src/test/kotlin/com/fiuba/asrekaf/AsrekafApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fiuba.asrekaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsrekafApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
