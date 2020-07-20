ea6c4a9ded58dc1c031dd764dda9e2ec2a49addc
Cristian Raña <cristian.rana@despegar.com>
2020-07-20 18:53:50 -0300

    HTTPS connection stablished

---
+++ src/main/kotlin/com/fiuba/asrekaf/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.fiuba.asrekaf
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+	//@Throws(Exception::class)
+	//override fun configure(http: HttpSecurity) {
+	//	http.requiresChannel().anyRequest().requiresSecure()
+	//}
+
+	//@Throws(Exception::class)
+	//override fun configure(http: HttpSecurity) {
+	//	http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+	//}
+
+	@Throws(Exception::class)
+	override fun configure(http: HttpSecurity) {
+		http.authorizeRequests().anyRequest().permitAll()
+		http.cors().and().csrf().disable()
+	}
+
+}
--- src/main/kotlin/com/fiuba/asrekaf/controller/UserController.kt
+++ src/main/kotlin/com/fiuba/asrekaf/controller/UserController.kt
@@ -2,20 +2,29 @@ package com.fiuba.asrekaf.controller
 
 import com.fiuba.asrekaf.api.ErrorMessage
 import com.fiuba.asrekaf.api.UserApiKey
-import com.fiuba.asrekaf.model.User
 import com.fiuba.asrekaf.api.UserCreation
 import com.fiuba.asrekaf.api.UserLogin
+import com.fiuba.asrekaf.model.User
 import com.fiuba.asrekaf.model.exception.DatabaseException
 import com.fiuba.asrekaf.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+
+@Controller
 @RestController
 @RequestMapping("/asrekaf")
 class UserController(@Autowired private val userService: UserService) {
+
+    @PostMapping("/")
+    fun index1(): ResponseEntity<String> = ResponseEntity.status(HttpStatus.OK).body("Index")
+
+    @GetMapping("/")
+    fun index(): ResponseEntity<String> = ResponseEntity.status(HttpStatus.OK).body("Index")
 
     @ExceptionHandler(value = [(DatabaseException::class)])
     fun exceptionHandler(): ResponseEntity<ErrorMessage> =
